9b40e604078cf6eb963c6363c086eac7a360d288
ghr85 <ghrenfrew@gmail.com>
2019-02-08 08:53:19 +0000

    created database schema, updated readme.md

---
+++ db/rex.sql
@@ -0,0 +1,20 @@
+DROP TABLE transactions;
+DROP TABLE merchants;
+DROP TABLE tags;
+
+CREATE TABLE tags(
+  id SERIAL4 PRIMARY KEY,
+  tag_name_str VARCHAR(255)
+);
+
+CREATE TABLE merchants(
+  id SERIAL4 PRIMARY KEY,
+  merchant_name_str VARCHAR(255)
+);
+
+CREATE TABLE transactions(
+  id SERIAL4 PRIMARY KEY,
+  tag_id_int INT4 REFERENCES tags(id),
+  merch_id_int INT4 REFERENCES merchants(id),
+  amount INT4
+);
