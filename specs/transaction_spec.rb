2214db58f1c8dc6cc5c688a58d0cb741ae6897ba
ghr85 <ghrenfrew@gmail.com>
2019-02-09 07:41:03 +0000

    refactored transaction spec test file to be dynamic

--- specs/transaction_spec.rb
+++ specs/transaction_spec.rb
@@ -4,7 +4,10 @@
 require('minitest/autorun')
 require('minitest/rg')
 require('pry')
+require_relative('../models/tag.rb')
+require_relative('../models/merchant.rb')
 require_relative('../models/transaction.rb')
+
 
 
 #Database CRUD actions are deemed to be tested by visual confirmation of valid data in Postico
@@ -12,22 +15,40 @@ require_relative('../models/transaction.rb')
 class TestTransaction < MiniTest::Test
 
 def setup
+  @test_tag = Tag.new(
+    {
+      'tag_name_str' => 'Food'
+    }
+  )
+  @test_tag.save()
+  @test_merchant = Merchant.new(
+    {
+      'merchant_name_str' => 'Tesco'
+    }
+  )
+  @test_merchant.save()
   @test_transaction = Transaction.new({
-      'tag_id_int' => 1,
-      'merchant_id_int' => 2,
+      'tag_id_int' => @test_tag.id,
+      'merchant_id_int' => @test_merchant.id,
       'amount_int' => 14
       })
+  @test_transaction.save()
 end
 
 def test_has_tag_id?
-  assert_equal(1,@test_transaction.tag_id_int)
+  assert_equal(@test_tag.id,@test_transaction.tag_id_int)
 end
 def test_has_merchant_id?
-  assert_equal(2,@test_transaction.merchant_id_int)
+  assert_equal(@test_merchant.id,@test_transaction.merchant_id_int)
 end
 def test_has_amount_int?
   assert_equal(14,@test_transaction.amount_int)
   # binding.pry
+end
+
+def test_merchant?
+    @test_transaction.save()
+    assert_equal('Tesco',@test_transaction.merchant.merchant_name_str)
 end
 
 end #class end
