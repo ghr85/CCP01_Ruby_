d1bd90c05475c189bb94eddc98f706a0887ac6d5
ghr85 <ghrenfrew@gmail.com>
2019-02-12 06:41:47 +0000

    created sort function in budget view/controller

--- controllers/budgets_controller.rb
+++ controllers/budgets_controller.rb
@@ -19,7 +19,30 @@ also_reload('../models/*')
 get '/budgets' do
   months = ['January','February','March','April','May','June','July','August','September','October','November','December']
   @month = months[params[:month].to_i - 1]
-  @budgets = Budget.analysis(params[:month],params[:year])
+  budgets = Budget.analysis(params[:month],params[:year])
   @year = params[:year].to_i
+  case params[:column]
+  when 'budget_num'
+    if params[:order] == 'ASC'
+      @budgets = budgets.sort_by{|budget| budget.budget_num.to_i}
+    else
+     @budgets = budgets.sort_by{|budget| budget.budget_num.to_i}.reverse
+   end
+
+ when 'tag_name_str'
+    if params[:order] == 'ASC'
+    @budgets = budgets.sort_by{|budget| budget.budget_name_str}
+  else
+    @budgets = budgets.sort_by{|budget| budget.budget_name_str}.reverse
+  end
+when 'total'
+   if params[:order] == 'ASC'
+   @budgets = budgets.sort_by{|budget| budget.total}
+ else
+   @budgets = budgets.sort_by{|budget| budget.total}.reverse
+ end
+  when nil
+  @budgets = budgets
+  end
 erb(:"budgets/index")
 end
