8e8553f15f8bfb7f48adfa9d6ed9e460d59ee4a9
ghr85 <ghrenfrew@gmail.com>
2019-02-13 12:56:53 +0000

    formatting tidy

--- models/budget.rb
+++ models/budget.rb
@@ -9,24 +9,24 @@ class Budget
 
   attr_reader :tag_name_str,:budget_num,:total
 
- def initialize(options)
-   @tag_name_str = options['tag_name_str']
-   @budget_num = options['budget_num']
-   @total = options['total']
- end
+  def initialize(options)
+    @tag_name_str = options['tag_name_str']
+    @budget_num = options['budget_num']
+    @total = options['total']
+  end
 
-    def self.analysis(month = '12', year = '2018')
-      sql ="SELECT
-      tags.tag_name_str, tags.budget_num, SUM(transactions.amount_num) AS total
-      from tags INNER JOIN transactions ON tags.id = transactions.tag_id_int
-      GROUP BY tags.tag_name_str, tags.budget_num, transactions.trans_date
-      HAVING EXTRACT(MONTH from trans_date) = $1 AND EXTRACT(YEAR from trans_date) = $2"
-      values=[month.to_i,year.to_i]
-      results = SqlRunner.run(sql,values)
-      return results.map { |budget| Budget.new(budget) } #returns an array of budget objects which are hashes
+  def self.analysis(month = '12', year = '2018')
+    sql ="SELECT
+    tags.tag_name_str, tags.budget_num, SUM(transactions.amount_num) AS total
+    from tags INNER JOIN transactions ON tags.id = transactions.tag_id_int
+    GROUP BY tags.tag_name_str, tags.budget_num, transactions.trans_date
+    HAVING EXTRACT(MONTH from trans_date) = $1 AND EXTRACT(YEAR from trans_date) = $2"
+    values=[month.to_i,year.to_i]
+    results = SqlRunner.run(sql,values)
+    return results.map { |budget| Budget.new(budget) } #returns an array of budget objects which are hashes
 
-    end
+  end
 
-    
 
-  end #class end
+
+end #class end
